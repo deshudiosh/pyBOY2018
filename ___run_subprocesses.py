450bf586b123eeace72f2277c77b5afe3b0a9c94
deshudiosh <deshudiosh@gmail.com>
2018-10-16 17:55:51 +0200

    boy2018

--- ___run_subprocesses.py
+++ ___run_subprocesses.py
@@ -5,7 +5,7 @@ from subprocess import Popen
 def gogogo():
     processes = []
     for i in range(cpu_count()-1):
-        processes.append(Popen("python %s" % "pyHIP2018.py", shell=True))
+        processes.append(Popen("python %s" % "pyBOY2018.py", shell=True))
 
     for process in processes:
         process.wait()
